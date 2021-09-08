874119cd4488225203fbcee4a065e3c8976cc2c0
kamkonphrom <kamkonphrom@gmail.com>
2021-09-08 21:03:11 +0700

    Add feature show hot movie

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,9 +1,15 @@
 import "./navbar.scss";
 import { Search, Notifications, ArrowDropDown } from "@material-ui/icons";
-
+import { useState } from "react";
 const Navbar = () => {
+  const [isScrolled, setIsScrolled] = useState(false);
+
+  window.onscroll = () => {
+    setIsScrolled(window.pageYOffset === 0 ? false : true);
+    return () => (window.onscroll = null);
+  }
   return (
-    <div className="navbar">
+    <div className={isScrolled ? "navbar scrolled" : "navbar"}>
       <div className="container">
         <div className="left">
           <img

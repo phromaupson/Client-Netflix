a99d066f39b728663a794d0a7f0eb43a18bc6518
kamkonphrom <kamkonphrom@gmail.com>
2021-10-02 21:22:29 +0700

    Add axios for request api

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,45 @@
 import "./app.scss"
-//import Login from "./pages/login/Login";
-//import Home from "./pages/home/Home";
+import Login from "./pages/login/Login";
+import Home from "./pages/home/Home";
 import Watch from "./pages/watch/Watch";
-//import Register from "./pages/register/Register"
+import Register from "./pages/register/Register"
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect
+} from "react-router-dom";
 
 const App = () => {
-    return <Watch / >
+    const user = true;
+    return (
+    <Router>
+        <Switch>
+            <Route exact path="/">
+                {user ? <Home /> : <Redirect to="/register"/> }
+            </Route>
+            <Route exact path="/register">
+                {!user ? <Register /> : <Redirect to="/"/> }
+            </Route>
+            <Route exact path="/login">
+                {!user ? <Login /> : <Redirect to="/"/> }
+            </Route>
+            { user && (
+                <>
+                <Route path="/movies">
+                    <Home type="movies"/>
+                </Route>
+                <Route path="/series">
+                    <Home type="series"/>
+                </Route>
+                <Route path="/watch">
+                    <Watch/>
+                </Route>
+                </>
+            )}
+        </Switch>
+    </Router>
+        );
 };
 
 export default App;
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,13 +1,37 @@
+import { useEffect, useState } from "react";
 import Featured from "../../components/featured/Featured";
 import List from "../../components/list/List";
 import Navbar from "../../components/navbar/Navbar";
 import "./home.scss";
+import axios from "axios";
 
-const Home = () => {
+const Home = ({type}) => {
+  const [lists, setLists] = useState([]);
+  const [genre, setGenre] = useState(null);
+
+  useEffect(() =>{
+    const getRandomLists = async ()=>{
+      try {
+        const res = await axios.get(
+          `lists${type ? "?type=" + type : ""}${genre ? "&genre=" + genre : ""}`
+        , {
+          headers: {
+            token: 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYxNDlmMDM1N2UzNzJkYzYwYmYzNjA5ZSIsImlzQWRtaW4iOnRydWUsImlhdCI6MTYzMzAwNzAwOCwiZXhwIjoxNjMzNDM5MDA4fQ.a8omTDlz551KCiUze9paH5WwhLBXAvjqhvl8LhOzMZw'
+          }
+        });
+        // console.log(res);
+        setLists(res.data);
+      } catch (err) {
+        console.log(err)
+      }
+    };
+    getRandomLists();
+  }, [type, genre]);
+
   return (
     <div className="home">
       <Navbar />
-      <Featured/>
+      <Featured type={type}/>
       <List/>
       <List/>
       <List/>
